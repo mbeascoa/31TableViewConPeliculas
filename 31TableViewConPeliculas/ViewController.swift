6b6f996fdbd039291dae013d3bce7fdbe9ec8ed8
mbeascoa <miguel.beascoabeastek.net>
2021-09-10 09:38:39 +0200

    10 septiembre

--- 31TableViewConPeliculas/ViewController.swift
+++ 31TableViewConPeliculas/ViewController.swift
@@ -7,12 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDataSource , UITableViewDelegate {
+    
+    
+    
+    var peliculas:[String]=[]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        peliculas=["amelie", "americanpie", "Atrapada", "basic", "bones", "Chicago", "dañocolateral", "daredevil", "desapariciones", "diablo", "diasdefutbol", "dostontosmuytontos"]
     }
+    
+    //número de filas a mostrar en el TableView, se sobreescribe el metodo
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peliculas.count
+    }
+    
+    
+    //ponemos en la celda una imagen y un texto
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell=UITableViewCell (style: UITableViewCell.CellStyle.subtitle, reuseIdentifier:"mycell")
+        cell.textLabel?.text = peliculas[indexPath.row]
+        cell.imageView?.image = UIImage(named: peliculas[indexPath.row])
+        return cell
+    }
+    
+
+    
 
 
 }
